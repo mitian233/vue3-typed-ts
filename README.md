50246a6dfb4b0842e1352a526f5ee886c798aa20
mitian233 <mitian233@yahoo.co.jp>
2024-01-28 22:46:45 +0800

    get ready to publish

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Vue3 Typed.js
+
+## Usage
+
+### Install
+
+```shell
+npm install vue3-typed-js typed.js sass
+```
+
+### Import
+
+```vue
+<script setup lang="ts">
+  import {VueTypedJs} from "vue3-typed-js";
+</script>
+
+<template>
+  <VueTypedJs
+    :strings="['Hello World', 'Hello Vue3']"
+    :typeSpeed="100"
+    :backSpeed="100"
+    :loop="true"
+  >
+    <span class="typing"></span>
+  </VueTypedJs>
+</template>
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(),dts({ rollupTypes: true })],
     build: {
         lib: {
             entry: resolve(__dirname, './packages/index.ts'),
             //fileName: (format) => `vue3-typed-js.${format}.ts`,
             name: 'Vue3TypedJs',
-            formats: ['es', 'umd'],
-            fileName: 'vue3-typed-js',
+            formats: ['es', 'umd', 'cjs','iife'],
+            fileName: 'index',
         },
         rollupOptions: {
             external: ['vue'],
